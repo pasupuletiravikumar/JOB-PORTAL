d1407d2766516c27da4860b07bf2928a3b787743
pasupuletiravikumar <149005025+pasupuletiravikumar@users.noreply.github.com>
2023-11-30 19:45:42 +0530

    Add files via upload

---
+++ object.JavaScript.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <p id = "abc"></p>
+        <script>
+        var person = {
+            first Name : "Rahul",
+            last Name : "Sharma",
+            age :50
+        };
+        document.getElementById("abc").innerHTML = person.firstName + "is" + person age + "years old.";
+        </script>
+    </body>
+</html>
